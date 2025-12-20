926ff8957bc43cb07e63394cb6a9b20157e8638d
eviltouch-work <eviltouch008@gmail.com>
2025-12-20 16:23:45 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,20 @@
 #just demo code for checking
 
-def demo():
-  print("ok it is working")
+def poor():
+  print("you are poor")
 
-demo()
+def middle_class():
+  print("you are middle class")
+
+def rich():
+  print("you are rich")
+
+def test(salary):
+  if salary>=100000:
+    rich()
+  elif salary>=10000 and salary<=100000:
+    middle_class()
+  else:
+    poor()
+
+test(70000)
